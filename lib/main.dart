d1de93a714d6e04622f13dbe7e1d46ee94da06fa
lighthead9288 <ivan9288@mail.ru>
2023-09-24 16:28:15 +0300

    count initial value 2

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 1;
+  int _counter = 2;
 
   void _incrementCounter() {
     setState(() {
